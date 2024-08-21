d992f3731a35bed01505ee6cd497bf8738ed431b
Moein Moeinnia <Moeinnia@protonmail.com>
2024-08-21 22:43:52 +0330

    feat: logger added

--- main.py
+++ main.py
@@ -18,7 +18,31 @@ from modules.text_processor import handle_text_datasource
 import time
 from confluent_kafka import Consumer, KafkaException
 
+import atexit
+import json
+import logging
+import logging.config
+import logging.handlers
+import pathlib
+
+from myLogger import MyJSONFormatter
+
 load_dotenv(override=True)
+
+logger = logging.getLogger("my_app")  # __name__ is a common choice
+
+
+def setup_logging():
+    config_file = pathlib.Path("logging_configs/stderr-json-file.json")
+    with open(config_file) as f_in:
+        config = json.load(f_in)
+
+    logging.config.dictConfig(config)
+    queue_handler = logging.getHandlerByName("queue_handler")
+    if queue_handler is not None:
+        queue_handler.listener.start()
+        atexit.register(queue_handler.listener.stop)
+
 
 consumer_conf = {'bootstrap.servers': os.getenv("KAFKA_SERVER"),
                  'security.protocol': 'SASL_SSL',
@@ -131,7 +155,23 @@ def consume_jobs(consumer, topic):
             print("Error handling message: %s", e)
 
 
+def main():
+    setup_logging()
+    logging.basicConfig(level="INFO")
+    logger.debug("debug message", extra={"x": "hello"})
+    logger.info("info message")
+    logger.warning("warning message")
+    logger.error("error message")
+    logger.critical("critical message")
+    try:
+        1 / 0
+    except ZeroDivisionError:
+        logger.exception("exception message")
+
+
 if __name__ == "__main__":
+
+    main()
 
     database_instance.connect()
 
