4960eb4be3587c33acd2fb9444231fb4bcb049c6
moein moeinnia <moein@pro.ai>
2024-08-24 12:13:04 +0330

    chore: new script added

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 # with bind mount
 docker run -d --name hamworker -v /var/logs/hamworker:/app/logs 
 
-#with volume mount
-docker run -d --name hamworker -v hamworker-logs:/app/logs
+# with volume mount
+docker run -d --name hamworker -v hamworker-logs:/app/logs --restart unless-stopped  hamyarchat/worker:latest
+
+#with bind mount 
+docker run -d --name hamworker -v ./logs:/app/logs --restart unless-stopped  hamyarchat/worker:latest
+
+# see volume content
+docker run --rm -it -v hamworker-logs:/data alpine /bin/sh
